a0748786e4e42e95f70fc8d2f570948c24aa69de
VaidasMakstutis <vaidas.makstutis@gmail.com>
2022-09-21 13:31:33 +0300

    Modified router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home'
-import NewPost from '@/components/NewPost'
 import notFound from '@/views/notFound'
 
 Vue.use(Router)
@@ -12,11 +11,6 @@ export default new Router({
       path: '/',
       name: 'posts-list',
       component: Home,
-    },
-    {
-      path: '/new-post',
-      name: 'new-post',
-      component: NewPost,
     },
     {
       path: '*',
