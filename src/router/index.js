7481b6b3d1092646c37977c1799175dc7de3b261
VaidasMakstutis <vaidas.makstutis@gmail.com>
2022-09-21 16:05:32 +0300

    posts.vue instead of home.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "@/views/home";
+import posts from "@/views/posts";
 import notFound from "@/views/notFound";
 
 Vue.use(Router);
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: "/",
       name: "posts-list",
-      component: Home,
+      component: posts,
     },
     {
       path: "*",
