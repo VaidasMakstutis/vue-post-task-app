6f42d006e869fe3204991c7a460ceec610e3d8fd
VaidasMakstutis <vaidas.makstutis@gmail.com>
2022-09-21 13:32:07 +0300

    Create New Post modal

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,29 @@
 <template>
    <div>
-    <!-- <button @click="$router.push('new-post')">Create New Post</button> -->
-    <!-- <router-link :to="{ name: 'new-post' }"> -->
-    <button class="button is-primary">
-      Create New Post
-    </button>
-    <!-- </router-link> -->
+     <NewPost v-if="showModal" @close="showModal = false"></NewPost>
+     <button class="button is-primary" @click="showModal = true">
+       Create New Post
+     </button>
     <h1 class="posts-list">Posts List:</h1>
   </div>
 </template>
 
 <script>
+import NewPost from '../components/NewPost.vue';
+
 export default {
-    name: 'posts-list',
-}
+  name: "posts-list",
+  components: { 
+      NewPost 
+  },
+
+  data() {
+    return {
+      showModal: false
+    };
+  }
+};
+
 </script>
 
 <style>
