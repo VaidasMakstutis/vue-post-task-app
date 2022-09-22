a2af4610b5c5751092852f511d7767a0068f8c20
VaidasMakstutis <vaidas.makstutis@gmail.com>
2022-09-22 13:47:48 +0300

    Post card

---
+++ src/views/postCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="card" v-for="post in posts" :key="post.id">
+      <header class="card-header">
+        <p class="card-header-title">{{ post.title }}</p>
+      </header>
+      <div class="card-content">
+        <div class="content">{{ post.body }}</div>
+      </div>
+      <footer class="card-footer">
+        <button class="button is-success">Edit</button>
+        <button class="button is-danger">Delete</button>
+        <button class="button is-info">Details</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["posts"],
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.card {
+  margin: 15px;
+}
+.card-header-title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  margin: 5px;
+}
+</style>
