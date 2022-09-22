572e0dd97b4292f548c8622f7f528286e894b67f
VaidasMakstutis <vaidas.makstutis@gmail.com>
2022-09-22 13:47:27 +0300

    Posts list

--- src/views/posts.vue
+++ src/views/posts.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
-    <NewPost v-if="showModal" @close="showModal = false"></NewPost>
+    <NewPost v-if="showModal" @close="showModal = false" />
     <button class="button is-primary" @click="showModal = true">
       Create New Post
     </button>
-    <h1 class="posts-list">Posts List:</h1>
+    <h1 class="posts-list">Posts</h1>
+    <postCard v-bind:posts="posts" />
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import NewPost from "../components/NewPost.vue";
+import postCard from "./postCard.vue";
 
 export default {
   name: "posts-list",
   components: {
     NewPost,
+    postCard,
   },
 
+  // state
   data() {
     return {
+      posts: [],
       showModal: false,
     };
   },
@@ -29,7 +34,8 @@ export default {
       await axios
         .get("http://localhost:3000/posts")
         .then((res) => {
-          console.log(res.data), (this.name = res.data);
+          this.posts = res.data;
+          // console.log("Posts[]:", this.posts);
         })
         .catch((error) => console.log(error));
     },
@@ -43,10 +49,9 @@ export default {
 
 <style>
 .posts-list {
-  border: 1px solid green;
   font-size: 24px;
   font-weight: 500;
   text-align: center;
-  margin-top: 25px;
+  margin: 25px 0;
 }
 </style>
