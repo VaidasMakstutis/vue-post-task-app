2eca04ac131679bbd346b1fee9104745fae17f81
VaidasMakstutis <vaidas.makstutis@gmail.com>
2022-09-27 12:00:03 +0300

    Validation and message "no created posts"

--- src/views/postCard.vue
+++ src/views/postCard.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="container">
-    <div class="card" v-for="post in posts" :key="post.id">
-      <header class="card-header">
-        <p class="card-header-title">{{ post.title }}</p>
-      </header>
-      <div class="card-content">
-        <div class="content">Author: {{ post.author }}</div>
-        <div class="content">
-          <div class="content" v-if="!post.updated_at">
-            Created at: {{ formatDate(post.created_at) }}
-          </div>
-          <div class="content" v-if="post.updated_at">
-            Updated at: {{ formatDate(post.updated_at) }}
+    <div v-if="!posts.length"><span class="no-created-posts">No created posts!</span></div>
+    <div class="card-container">
+      <div class="card" v-for="post in posts" :key="post.id">
+        <header class="card-header">
+          <p class="card-header-title">{{ post.title }}</p>
+        </header>
+        <div class="card-content">
+          <div class="content">Author: {{ post.author }}</div>
+          <div class="content">
+            <div class="content" v-if="!post.updated_at">
+              Created at: {{ formatDate(post.created_at) }}
+            </div>
+            <div class="content" v-if="post.updated_at">
+              Updated at: {{ formatDate(post.updated_at) }}
+            </div>
           </div>
         </div>
+        <footer class="card-footer">
+          <button class="button is-info">Details</button>
+          <button class="button is-success">Edit</button>
+          <button class="button is-danger">Delete</button>
+        </footer>
       </div>
-      <footer class="card-footer">
-        <button class="button is-info">Details</button>
-        <button class="button is-success">Edit</button>
-        <button class="button is-danger">Delete</button>
-      </footer>
     </div>
   </div>
 </template>
@@ -34,7 +37,16 @@ export default {
 </script>
 
 <style>
-.container {
+.no-created-posts {
+  font-size: 30px;
+  font-weight: 500;
+  margin-top: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.card-container {
   display: flex;
   flex-wrap: wrap;
 }
