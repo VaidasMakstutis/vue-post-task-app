9c341dc7915664889275609bb11e51f4aca2b756
VaidasMakstutis <vaidas.makstutis@gmail.com>
2022-09-28 18:08:33 +0300

    Delete post modal

---
+++ src/components/DeletePost.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Are you sure that you want to delete this post?</p>
+        <button class="delete" aria-label="close" @click="$emit('close')"></button>
+      </header>
+      <footer class="modal-card-foot">
+        <button type="submit" class="button is-success" @click="confirmDeletePost()">
+          Confirm delete
+        </button>
+        <button class="button" @click="$emit('close')">Cancel</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "DeletePost",
+  props: { id: Number },
+  data() {
+    return {};
+  },
+  methods: {
+    async confirmDeletePost() {
+      try {
+        await axios.delete("http://localhost:3000/posts/" + this.id).then((res) => {
+          return res.data;
+        });
+        this.$emit("toggleDeleteModal");
+        this.$router.go();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
